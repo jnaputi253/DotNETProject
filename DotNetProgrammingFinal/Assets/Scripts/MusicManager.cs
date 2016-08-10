ba7a28e01bc2fe1b78aac9b753b005e36814970b
Hippeius <jnaputi509@gmail.com>
2016-08-10 14:23:54 -0700

    Stuff

---
+++ DotNetProgrammingFinal/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	public static MusicManager instance = null;
+	public AudioSource musicSource;
+
+	private void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else if(instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void PlaySingle(AudioClip clip) {
+		musicSource.clip = clip;
+		musicSource.Play ();
+	}
+
+	public void RandomizeSfx(AudioClip[] clips) {
+		int random = Random.Range (0, clips.Length);
+		musicSource.clip = clips [random];
+		musicSource.Play ();
+	}
+}
